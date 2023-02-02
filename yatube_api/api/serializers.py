ce6f66ed35209b4042a60bc74fc4c0a8a64ff76e
Olesya Sim <leka4581698@gmail.com>
2023-02-02 17:10:57 +0300

    до проверки

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
+from posts.models import Comment, Follow, Group, Post
 
-from posts.models import Comment, Post
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,3 +24,31 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Group
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    following = SlugRelatedField(slug_field='username',
+                                 queryset=User.objects.all())
+    user = SlugRelatedField(slug_field='username', read_only=True,
+                            default=serializers.CurrentUserDefault())
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                fields=['user', 'following'],
+                queryset=Follow.objects.all()
+            )]
+
+    def validate_following(self, value):
+        if value == self.context['request'].user:
+            raise serializers.ValidationError()
+        return value
